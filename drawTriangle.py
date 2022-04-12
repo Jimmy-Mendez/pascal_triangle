23d6410fb2973ab127b6b290efbaeb10317e5e5c
Jimmy Mendez <jimmymendez@Jimmys-MacBook-Air.local>
2022-04-12 11:44:16 -0400

    update py file

--- drawTriangle.py
+++ drawTriangle.py
@@ -1,5 +1,25 @@
 import turtle
 
-triangle = turtle.turtle()
+triangle = turtle.Turtle()
+triangle.penup()
+triangle.goto(0,200)
+
+
+f = open("TRIANGLE.txt", "r")
+
+for line in f:
+    writeLine = str(line)
+    writeLine = writeLine.replace(" ", "\t")
+    triangle.right(90)
+    triangle.forward(50)
+    triangle.right(90)
+    triangle.forward(25)
+    triangle.left(180)
+    triangle.pendown()
+    triangle.write(writeLine)
+    triangle.penup()
+    
+
 
 # use turtle to draw triangle using the TRIANGLE.txt file
+
